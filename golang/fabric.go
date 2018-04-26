1f1e881e95725995539bbaf65d676916b6ac4c70
davidliu <david-khala@hotmail.com>
2018-04-26 11:30:51 +0800

    KV2json

--- golang/fabric.go
+++ golang/fabric.go
@@ -3,6 +3,7 @@ package golang
 import (
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"github.com/hyperledger/fabric/protos/ledger/queryresult"
+	"encoding/json"
 )
 
 func WorldStates(stub shim.ChaincodeStubInterface) ([]queryresult.KV, error) {
@@ -21,4 +22,14 @@ func WorldStates(stub shim.ChaincodeStubInterface) ([]queryresult.KV, error) {
 		kvs = append(kvs, *kv)
 	}
 	return kvs, nil
+}
+func KV2Json(kv queryresult.KV) (string) {
+	type KVJson struct {
+		Namespace string
+		Key       string
+		Value     string
+	}
+	kvJson:= KVJson{Namespace:kv.Namespace,Key:kv.Key,Value:string(kv.Value)}
+	jsonString,_:=  json.Marshal(kvJson)
+	return string(jsonString)
 }
