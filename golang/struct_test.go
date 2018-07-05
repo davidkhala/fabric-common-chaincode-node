f5115d8b2b6918146cf421274e1a7e09564a8c1d
davidliu <david-khala@hotmail.com>
2018-07-05 11:46:05 +0800

    fully tested

--- golang/struct_test.go
+++ golang/struct_test.go
@@ -7,8 +7,13 @@ import (
 
 func TestSet_Has(t *testing.T) {
 	var v1 = "abc"
+	var v2 = "bcd"
+	var v3 = "not"
 	var set1 = Set{};
 	set1.Put(v1)
-
+	set1.Put(v2);
 	testutil.AssertEquals(t, set1.Has(v1), true)
+	testutil.AssertEquals(t, set1.Has(v3), false)
+	var list = set1.List();
+	testutil.AssertEquals(t, len(list),2)
 }
