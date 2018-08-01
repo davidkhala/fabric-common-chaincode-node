d65b502bfb138918a7db5f5974b6f2541eda4bed
davidliu <david-khala@hotmail.com>
2018-08-01 18:26:09 +0800

    add put/get PrivateData Buffer wrapper

--- base.js
+++ base.js
@@ -13,7 +13,40 @@ exports.Base = class {
 	 * @param {string} payload A payload can be used to include data about the event
 	 */
 	setEvent(name, payload) {
-		this.stub.setEvent(name,Buffer.from(payload))
+		this.stub.setEvent(name, Buffer.from(payload));
+	}
+	/**
+	 * getPrivateData returns the value of the specified `key` from the specified
+	 * `collection`. Note that GetPrivateData doesn't read data from the
+	 * private writeset, which has not been committed to the `collection`. In
+	 * other words, GetPrivateData doesn't consider data modified by PutPrivateData
+	 * that has not been committed.
+	 *
+	 * @param {string} collection The collection name
+	 * @param {string} key Private data variable key to retrieve from the state store
+	 * @returns {string} value
+	 */
+	async getPrivateData(collection, key) {
+		const raw = await this.stub.getPrivateData(collection, key);
+		return raw.toString();
+	}
+	/**
+	 * putPrivateData puts the specified `key` and `value` into the transaction's
+	 * private writeset. Note that only hash of the private writeset goes into the
+	 * transaction proposal response (which is sent to the client who issued the
+	 * transaction) and the actual private writeset gets temporarily stored in a
+	 * transient store. PutPrivateData doesn't effect the `collection` until the
+	 * transaction is validated and successfully committed. Simple keys must not be
+	 * an empty string and must not start with null character (0x00), in order to
+	 * avoid range query collisions with composite keys, which internally get
+	 * prefixed with 0x00 as composite key namespace.
+	 *
+	 * @param {string} collection The collection name
+	 * @param {string} key Private data variable key to set the value for
+	 * @param {string} value Private data variable value
+	 */
+	async putPrivateData(collection, key, value) {
+		return this.stub.putPrivateData(collection, key, Buffer.from(value));
 	}
 	static Success(data) {
 		return exports.shim.success(Buffer.from(data));
